050fa01012c0e9c56f63bae7b7e3210c000b406b
Maj Zabukovnik <maj.zabukovnik1@gmail.com>
2023-05-26 10:52:34 +0300

    adding basic html form

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>First page</title>
+</head>
+<body>
+<h1>Enter selected date range</h1>
+
+<form method="POST">
+    <label for="start_d">Enter the beginning date</label><br>
+    <input type="date" name="start_d"><br><br>
+
+    <label for="end_d">Enter the ending date</label><br>
+    <input type="date" name="end_d"><br><br>
+
+    <button type="submit">Submit</button>
+</form>
+</body>
+</html>
