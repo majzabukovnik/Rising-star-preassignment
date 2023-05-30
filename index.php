f0b003f3639b8c9a57dd0a13f622cd640168b441
Maj Zabukovnik <maj.zabukovnik1@gmail.com>
2023-05-30 08:37:53 +0300

    adding css classes to html elements

--- index.php
+++ index.php
@@ -48,9 +48,9 @@ if (isset($_POST['start_d']) && validator()) {
     <link rel="stylesheet" type="text/css" href="views/css/main.css">
 </head>
 <body>
-<h1>Enter selected date range</h1>
+<h1 class="naslov">Enter selected date range</h1>
 
-<form method="POST">
+<form method="POST" class="lamal">
     <label for="start_d">Enter the beginning date</label><br>
     <input type="date" name="start_d" min="2020-01-01" max="2020-12-31"><br><br>
 
@@ -64,7 +64,7 @@ if (isset($_POST['start_d']) && validator()) {
     <input type="checkbox" id="greatDeal" name="mode[]" value="greatDeal">
     <label for="vehicle3">Get the best days to buy and sell</label><br><br>
 
-    <button type="submit">Submit</button>
+    <button type="submit" class="majmun">Submit</button>
 </form>
 <?php require_once __DIR__ . '/views/partials/dataDisplay.php'; ?>
 </body>
